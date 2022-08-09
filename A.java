11c1799ff5bb87a57bdf90a05c847d9d8b0298b6
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-08-09 16:34:30 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+class A{
+	static int c=30;
+	int a=20;
+	public static void main(String ...args){
+	int b=10;
+	
+System.out.println(A.c);
+System.out.println(b);
+A obj = new A();
+System.out.println(obj.a);
+}
+}
+ 
