001c89d573a818ff6eeeaa453d4c16a26864034a
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-08-10 15:40:42 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,13 @@
+class Square{
+	public static void main(String[] args){
+	System.out.println("Enter side:");          
+	int side = 10;                                                     //initialize the value of side of a square
+	
+
+	System.out.println(+side);
+	int perimeter = 4*side;                                            // logic for perimeter of the square
+	int area = side*side;                                             // logic for the area of the square
+	System.out.println("The perimeter of square is:=" +perimeter);   // output 
+	System.out.println("The area of square is:=" +area);            //output 
+}
+}
