db11d9e172363658004fc98a04209785b2840c2c
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-08-22 14:03:44 +0530

    Add files via upload

---
+++ control.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class control {
+	public static void main(String[] args) {
+		int x = 13;
+		int y = 3;
+		if(x+y<10)
+			System.out.println("x+y is less than 10");
+		
+		else {
+			System.out.println("x+y is greater than 10");
+		}
+	
+}
+
+}
