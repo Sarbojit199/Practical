e9b326a7572d9b932a8ecb456b909ad094ba6454
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-08-22 14:04:43 +0530

    Add files via upload

---
+++ ifelseladder.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class ifelseladder {
+	public static void main(String[] args) {
+		String name = "Neha";
+		if(name=="Meena") {
+			System.out.println("Name is Meena");
+		}
+		else if(name=="Neha") {
+			System.out.println("Name is neha");
+		}
+		else if(name=="Amit") {
+			System.out.println("Name is Amit");
+		}
+		else {
+			System.out.println(name);
+		}
+	}
+}
