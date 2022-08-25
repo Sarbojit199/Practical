f93b3975363e77fed47ba28b3c4866d497695b13
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-08-25 14:11:40 +0530

    Add files via upload

---
+++ switchvowel.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class switchvowel {
+		public static void main(String[] args) {
+			char ch='a';
+			switch(ch) {
+			case 'a': System.out.println("its a vowel");
+			break;
+			case 'e': System.out.println("its a vowel");
+			break;
+			case 'o': System.out.println("its a vowel");
+			break;
+			default:System.out.println("consonent");
+			
+			}
+		}
+}
