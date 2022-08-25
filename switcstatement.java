9f1bccae631640c5e2982873980bc9993ae2aa82
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-08-25 14:12:04 +0530

    Add files via upload

---
+++ switcstatement.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class switcstatement {
+		public static void main(String[] args) {
+			int num =10;
+			switch(num) {
+			case 1:System.out.println("12");
+			break;
+			case 12:System.out.println("20");
+			break;
+			case 10:System.out.println("10");
+			break;
+			case 15:System.out.println("15");
+			break;
+			//default case statement
+			default: System.out.println("number is not within our range");
+			}
+		}
+}
