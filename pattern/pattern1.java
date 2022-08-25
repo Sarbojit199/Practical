9c55400bfd29818cdbf141d807831f0e14d13881
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-08-25 14:08:32 +0530

    Add files via upload

---
+++ pattern/pattern1.java
@@ -0,0 +1,20 @@
+package Java;
+
+public class pattern1 {
+	public static void main(String args[]) {
+/*	*
+	**
+	***
+	****
+	*****			*/
+	for(int i=0;i<6;i++) {
+		for(int j=0; j<i; j++) {
+			System.out.print("*");
+		}
+		System.out.println();		//new line
+	}
+	
+	}
+	
+	
+}
