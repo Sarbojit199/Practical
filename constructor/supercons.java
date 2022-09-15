5471033e190f1b972552d6dba023d97c955a6d9f
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-09-15 14:33:50 +0530

    Add files via upload

---
+++ constructor/supercons.java
@@ -0,0 +1,19 @@
+/*  constructor */
+class shape1{
+	public shape1() {
+	System.out.println("hello");
+	}
+}
+class size2 extends shape1{
+	size2(){
+	System.out.println("hi");
+}}
+public class supercons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+size2 obj= new size2();
+
+	}
+
+}
