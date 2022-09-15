985a3f21402d712e56d82371af4ea284c20749a7
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-09-15 14:30:23 +0530

    Add files via upload

--- constructor/Shape.java
+++ constructor/Shape.java
@@ -1,10 +1,38 @@
+/* Super constructor */
+class Shap{
+	String name="circle";
+	void print() {
+		System.out.println("print circle");
+	}
+}
+class size extends Shap{
+	String name ="triangle";	//data member of child method
+			
 
+void displayName() {
+	System.out.println(name);
+	System.out.println(super.name);		//calling the super constructor of parent class
+}
 
+void print() {
+System.out.println("print triangle");
+super.print();			//calling the super constructor
+}
+}
 public class Shape {
 	
 
-		public static void main(String[] args) {
-			Circlee circle1 =new Circlee();
+		public char[] name;
 
-}
-}
+		public static void main(String[] args) {
+			size obj= new size();
+			obj.displayName();
+			obj.print();
+			
+			//output-  
+		/*	triangle
+		 * circle
+		 * print triangle
+		 * print circle */
+		 
+}}
