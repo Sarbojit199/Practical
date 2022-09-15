ceff1cde39240704586b796d42738c97785a89bf
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-09-15 14:47:32 +0530

    Add files via upload

---
+++ Addresshasa.java
@@ -0,0 +1,13 @@
+package com.cognizant.shapes;
+
+public class Addresshasa {
+public String city, state,country;
+
+public Addresshasa(String city, String state, String country) {
+	super();
+	this.city = city;
+	this.state = state;
+	this.country = country;
+}
+
+}
