b779bb53c45ba554523e4b5c4c2299c9afc6e84d
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-10-17 17:55:42 +0530

    Add files via upload

---
+++ Exceptionhandeling/trycatch.java
@@ -0,0 +1,16 @@
+package com.cognizant.tax;
+
+public class trycatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int num=25/0;
+			
+		} catch (Exception e) {
+			System.out.println("Arithmetic exception occured");
+		}
+
+	}
+
+}
